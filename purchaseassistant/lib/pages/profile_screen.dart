caaa13a9cb2fde20d09519aed7fda44bdda2be21
khoffan <khoffan222@gmail.com>
2023-08-21 22:38:44 +0700

    profile update data

--- purchaseassistant/lib/pages/profile_screen.dart
+++ purchaseassistant/lib/pages/profile_screen.dart
@@ -2,6 +2,7 @@
 import 'dart:typed_data';
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -22,7 +23,13 @@ class ProfileScreenApp extends StatefulWidget {
 
 class _ProfileScreenAppState extends State<ProfileScreenApp> {
   final Future<FirebaseApp> _firebase = Firebase.initializeApp();
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final auth = FirebaseAuth.instance;
+
+  String name = "";
+  String bio = "";
+  String image = "";
+
   Uint8List? _image;
   void selectImage() async {
     Uint8List img = await pickerImage(ImageSource.gallery);
@@ -33,93 +40,129 @@ class _ProfileScreenAppState extends State<ProfileScreenApp> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 242, 195, 245),
-        actions: [
-          IconButton(
-            onPressed: () {
-              showMediabottom(context);
-            },
-            icon: Icon(Icons.dehaze),
-          )
-        ],
-      ),
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Stack(
-                alignment: AlignmentDirectional.bottomCenter,
-                children: [
-                  _image != null
-                      ? CircleAvatar(
-                          radius: 64,
-                          backgroundImage: MemoryImage(_image!),
-                        )
-                      : const CircleAvatar(
-                          radius: 64,
-                          backgroundImage: NetworkImage(
-                              'https://www.google.com/imgres?imgurl=https%3A%2F%2Fw7.pngwing.com%2Fpngs%2F205%2F731%2Fpng-transparent-default-avatar-thumbnail.png&tbnid=vj1POnmqwlZL-M&vet=12ahUKEwiv1vzRpZqAAxX-0qACHdgLBcgQMygCegUIARDlAQ..i&imgrefurl=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Ddefault&docid=J354HYBi_egj6M&w=360&h=360&q=default%20avatar%20in%20png&hl=en&ved=2ahUKEwiv1vzRpZqAAxX-0qACHdgLBcgQMygCegUIARDlAQ'),
-                        ),
-                  Positioned(
-                    child: IconButton(
-                      onPressed: () {
-                        // selectImage();
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                              builder: (context) => EditProfile()),
-                        );
-                      },
-                      icon: Icon(
-                        Icons.add_a_photo,
-                        size: 28,
-                      ),
-                    ),
-                    bottom: -10,
-                    left: 80,
+    return StreamBuilder(
+        stream: _firestore.collection('userProfile').snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("Error: ${snapshot.error}"),
+            );
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasData) {
+            List<QueryDocumentSnapshot> document = snapshot.data!.docs;
+            for (var doc in document) {
+              Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+              name = data?["name"] ?? '';
+              bio = data?["bio"] ?? '';
+              image = data?["imageLink"] ?? '';
+            }
+            return Scaffold(
+              appBar: AppBar(
+                backgroundColor: Color.fromARGB(255, 242, 195, 245),
+                actions: [
+                  IconButton(
+                    onPressed: () {
+                      showMediabottom(context);
+                    },
+                    icon: Icon(Icons.dehaze),
                   )
                 ],
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              // Center(
-              //   child: SizedBox(
-              //     width: 200,
-              //     child: ElevatedButton(
-              //       onPressed: () {
-              //         Navigator.of(context).push(
-              //           MaterialPageRoute(builder: (context) => EditProfile()),
-              //         );
-              //       },
-              //       child: Text("Edit profile"),
-              //     ),
-              //   ),
-              // ),
-              Center(
-                child: Column(children: [
-                  Text(
-                    "ข้อมูลผู้ใช้",
-                    style:
-                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              body: Container(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Stack(
+                        alignment: AlignmentDirectional.bottomCenter,
+                        children: [
+                          _image != null
+                              ? CircleAvatar(
+                                  radius: 64,
+                                  backgroundImage: NetworkImage(image),
+                                )
+                              : const CircleAvatar(
+                                  radius: 64,
+                                  backgroundImage: NetworkImage(
+                                      'https://www.google.com/imgres?imgurl=https%3A%2F%2Fw7.pngwing.com%2Fpngs%2F205%2F731%2Fpng-transparent-default-avatar-thumbnail.png&tbnid=vj1POnmqwlZL-M&vet=12ahUKEwiv1vzRpZqAAxX-0qACHdgLBcgQMygCegUIARDlAQ..i&imgrefurl=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Ddefault&docid=J354HYBi_egj6M&w=360&h=360&q=default%20avatar%20in%20png&hl=en&ved=2ahUKEwiv1vzRpZqAAxX-0qACHdgLBcgQMygCegUIARDlAQ'),
+                                ),
+                          Positioned(
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.add_a_photo,
+                                size: 28,
+                              ),
+                            ),
+                            bottom: -10,
+                            left: 80,
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => EditProfile(),
+                                ),
+                              );
+                            },
+                            child: Text("Edit profile"),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "ข้อมูลผู้ใช้",
+                            style: TextStyle(
+                                fontSize: 20.0, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          Text(
+                            "ชื่อ ${name}",
+                            style: TextStyle(
+                                fontSize: 20.0, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          Text(
+                            "bio ${bio}",
+                            style: TextStyle(
+                                fontSize: 20.0, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 20.0,
+                          ),
+                          // Text(auth.currentUser.email!),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                    ],
                   ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  // Text(auth.currentUser.email!),
-                ]),
+                ),
               ),
-              const SizedBox(
-                height: 10,
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+            );
+          }
+          return Container();
+        });
   }
 
   Future showMediabottom(context) {
@@ -148,7 +191,7 @@ class _ProfileScreenAppState extends State<ProfileScreenApp> {
                           auth.signOut().then((value) {
                             Navigator.pushReplacement(context,
                                 MaterialPageRoute(builder: (context) {
-                              return  LoginScreen();
+                              return LoginScreen();
                             }));
                             // Navigator.of(context).pushAndRemoveUntil(
                             //     MaterialPageRoute(
