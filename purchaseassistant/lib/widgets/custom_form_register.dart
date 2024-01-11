11e4c00ec8c3eb5827569800ba4578d41b5d0831
khoffan <khoffan222@gmail.com>
2024-01-11 21:38:04 +0700

    fix error custom_register

--- purchaseassistant/lib/widgets/custom_form_register.dart
+++ purchaseassistant/lib/widgets/custom_form_register.dart
@@ -154,105 +154,12 @@ class _CustomFormRegisterState extends State<CustomFormRegister> {
                             }
                           }
                         },
-<<<<<<< HEAD
-                      ),
-                      const SizedBox(height: 15.0),
-                      TextFormField(
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          filled: true,
-                          fillColor: Color.fromARGB(190, 255, 255, 255),
-                          hintText: 'Password',
-                          border: OutlineInputBorder(),
-                          labelText: "Password",
-                        ),
-                        onSaved: (String? password) {
-                          widget.register.password = password;
-                        },
-                        validator: (String? value) {
-                          return (value == null || value.isEmpty)
-                              ? 'กรุณาใส่รหัสผ่าน'
-                              : null;
-                        },
-                      ),
-                      const SizedBox(height: 20.0),
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.circular(20), // <-- Radius
-                            ),
-                            backgroundColor: Colors.green,
-                            padding: const EdgeInsetsDirectional.symmetric(
-                              horizontal: 40,
-                              vertical: 8,
-                            ),
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          onPressed: () async {
-                            if (widget.formKey.currentState!.validate()) {
-                              widget.formKey.currentState?.save();
-                              try {
-                                if (widget.register.email != "" &&
-                                    widget.register.password != "") {
-                                  await AuthServices().registerEmailandPassword(
-                                      widget.register.email!,
-                                      widget.register.password!);
-                                }
-                                widget.formKey.currentState?.reset();
-                                if (context.mounted) {
-                                  Navigator.pushReplacementNamed(
-                                      context, AppRoute.login);
-                                }
-                                Fluttertoast.showToast(
-                                  msg: "สร้างบัญชีผู้ใช้สำเร็จ",
-                                  gravity: ToastGravity.CENTER,
-                                );
-                              } on FirebaseAuthException catch (e) {
-                                String message = '';
-                                if (e.code == 'email-already-in-use') {
-                                  message = 'มีชื่อบัญชีผู้ใช้นี้แล้ว';
-                                } else if (e.code == 'weak-password') {
-                                  message =
-                                      'รหัสผ่านต้องมีความยาว 6 ตัวอักษรขึ้นไป';
-                                } else {
-                                  message = e.message!;
-                                }
-                                Fluttertoast.showToast(
-                                    msg: message, gravity: ToastGravity.CENTER);
-                              }
-                            }
-                          },
-                          child: const Text("Register")),
-                    ],
-                  )),
-=======
                         child: const Text("Register")),
                   ],
                 )),
->>>>>>> 10df0deee2cfe528850ca98dab4d9d50c90aff13
 
             const Spacer(),
 
-<<<<<<< HEAD
-              // Register button
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("Already have an account?"),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, AppRoute.login);
-                    },
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                        color: Colors.green,
-                      ),
-=======
             // Register button
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -266,7 +173,6 @@ class _CustomFormRegisterState extends State<CustomFormRegister> {
                     "Login",
                     style: TextStyle(
                       color: themeError,
->>>>>>> 10df0deee2cfe528850ca98dab4d9d50c90aff13
                     ),
                   ),
                 )
